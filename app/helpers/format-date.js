148511ca5dc3e056fd6083006380139d79871a88
angmas <angela.mashlan@safeco.com>
2017-06-06 14:18:22 -0400

    Adds helper to do user-friend formatting of ISO date
    -updated README to include ember-moment and ember-cli-moment-shim as
    dependencies
    -installed ember-moment, ember-cli-moment-shim and moment
    -created a helper format-date for date formatting using ember
    generate helper
    -updated researches/index.hbs to invoke helper and pass params

---
+++ app/helpers/format-date.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+// export function formatDate(params, hash) {
+//   compute(params, hash) {
+//     let newDate
+//     if (hash.formatType === 'datetime') {
+//       newDate = moment(params).format('MM/DD/YYYY h:mm:ss a');
+//     } else if (has.formatType == 'date') {
+//       newDate = moment(params).format('MM/DD/YYYY');
+//     } else {
+//       newDate = params
+//     }
+//     return newDate;
+//   }
+// }
+
+export default Ember.Helper.extend({
+  compute(params, hash) {
+    {
+      let formattedDate;
+      console.log('params: ', params)
+      console.log('hash: ', hash)
+      if (hash.formatType === 'datetime') {
+        formattedDate = moment(params, moment.ISO_8601).format('MM/DD/YYYY h:mm:ss a');
+      } else if (hash.formatType === 'date') {
+        formattedDate = moment(params, moment.ISO_8601).format('MM/DD/YYYY');
+      } else {
+        formattedDate = params;
+      }
+      return formattedDate;
+    }
+  }
+});
