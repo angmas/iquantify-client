726ba4eaf4f2d612e0b323fc3914a02e897851b4
angmas <angela.mashlan@safeco.com>
2017-06-07 07:47:35 -0400

    Fixes table layout issue
    -First column in the research table was being displayed too wide
    -This was due to ember automatically inserting a div tag between
    the table component and list component. By defining the tagName
    in the list component, ember created the tr tags for me instead of
    divs.

--- app/components/researches/research-list.js
+++ app/components/researches/research-list.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'tr',
   actions: {
       researchDelete () {
         // console.log('research-list this: ', this.get('research') );
