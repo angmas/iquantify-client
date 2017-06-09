4301b679f159442b8e23b1e6ce3510cc4792d931
angmas <angela.mashlan@safeco.com>
2017-06-08 22:59:12 -0400

    Adds create quantum record functionality
    -used ember generate to create an adapter to customize post route
    for quantum model.
    -updated button actions with the correct action labels
    -updated research-count component to get the quantum parent object.

--- app/components/researches/research-count.js
+++ app/components/researches/research-count.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  quantumPlusOne: {
+    count: 1
+  },
+  quantumMinusOne: {
+    count: -1
+  },
+  actions: {
+    createQuantumPlusOne() {
+      let data = this.get('quantumPlusOne');
+      data.research = this.get('research');
+      this.sendAction('createQuantum', data);
+    },
+    createQuantumMinusOne() {
+      let data = this.get('quantumMinusOne');
+      data.research = this.get('research');
+      this.sendAction('createQuantum', data);
+    }
+  }
 });
